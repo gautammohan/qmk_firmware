c1926e4de309493ac324d77d9e162e5981191863
Gautam Mohan <s.gautam.mohan@gmail.com>
2020-11-17 22:36:20 -0500

    add shift_tab key because tab and shift are on the same key

--- keyboards/kyria/keymaps/gautammohan/keymap.c
+++ keyboards/kyria/keymaps/gautammohan/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |ArrCtrl |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |        |
+ * |        |   Z  |   X  |   C  |   V  |   B  |Sh_Tab|      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        |      |      | Tab  |Bckspc| Esc  |  | Enter| Space| Del  |      |      |
  *                        |      |      | Shift|Ctrl  | GUI  |  |CtlAlt| Alt  | Symb |      |      |
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
       XXXXXXX,       KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   XXXXXXX,
       MO(_ARRCTRL),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      XXXXXXX,       KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+      XXXXXXX,       KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,  S(KC_TAB),   XXXXXXX, XXXXXXX, XXXXXXX, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
                 XXXXXXX, XXXXXXX, LSFT_T(KC_TAB), LCTL_T(KC_BSPC), LGUI_T(KC_ESC), LCA_T(KC_ENT), RALT_T(KC_SPC), LT(_NUMSYMB, KC_DEL), MO(_RGBFN), XXXXXXX
     ),
 /*
